23345af4f8839edf3a2c070cbf0ff38fa05c8539
zheng030 <99647402+zheng030@users.noreply.github.com>
2025-12-15 22:20:00 +0800

    Update data.js

--- frontend/data.js
+++ frontend/data.js
@@ -50,7 +50,7 @@ const menuData = {
     },
 
     "主餐": [{ name: "炒飯", price: 90 }, { name: "蒜漬糖蜜番茄麵包", price: 140 }, { name: "日式炒烏龍麵", price: 150 }, { name: "親子丼", price: 160 }, { name: "酒蒸蛤蠣", price: 180 }, { name: "純酒白蝦", price: 200 }, { name: "唐揚咖哩", price: 220 }, { name: "龍膽石斑魚湯", price: 280 }, { name: "味繒鮭魚", price: 300 }],
-    "炸物": [{ name: "嫩炸豆腐", price: 80 }, { name: "脆薯", price: 100 }, { name: "雞塊", price: 100 }, { name: "鑫鑫腸", price: 100 }, { name: "雞米花", price: 100 }, { name: "洋蔥圈", price: 100 }, { name: "酥炸魷魚", price: 200 }, { name: "炸物拼盤", price: 400 }],
+    "炸物": [{ name: "嫩炸豆腐", price: 80 }, { name: "脆薯", price: 100 }, { name: "雞塊", price: 100 }, { name: "鑫鑫腸", price: 100 }, { name: "雞米花", price: 100 }, { name: "洋蔥圈", price: 100 },  { name: "香酥雞胸", price: 120 },{ name: "酥炸魷魚", price: 200 }, { name: "炸物拼盤", price: 400 }],
     "厚片": [{ name: "花生厚片", price: 80 }, { name: "奶酥厚片", price: 80 }, { name: "蒜香厚片", price: 80 }, { name: "巧克力厚片", price: 80 }, { name: "巧克力棉花糖厚片", price: 80 }],
     "甜點": [{ name: "起司蛋糕", price: 120 }],
     "其他": [{ name: "服務費", price: 100 }, { name: "清潔費", price: 300 }, { name: "碎碎平安", price: 500 }]
